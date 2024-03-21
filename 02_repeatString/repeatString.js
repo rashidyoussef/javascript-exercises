d0d7a52be2849b5f8c8ff60633b40841849b5078
rashidyoussef <rashid_gaber@hotmail.com>
2024-03-21 16:07:45 -0600

    practicando

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
+const repeatString = function(word,times) {
+    let resul = "";
+    if (times < 0){
+        resul = "ERROR";
+    }
+    else
+    for (i = 0; i < times; i++){
+        resul += word;
+    }
+    return resul;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let letras = string.split('');
+let palabra = letras.reverse();
+let palabraR = palabra.join('');
+return palabraR;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
