3b7e8504037a619b287d916da324ba48e2bbb3e2
rashidyoussef <rashid_gaber@hotmail.com>
2024-03-21 17:02:33 -0600

    debo seguir practicando

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(bot,top) {
+      
+    if (bot > top) {
+num1 = top;
+num2 = bot;
+    }
 
+    else {
+num2 = top;
+num1 = bot;
+    }
+    let suma = 0;
+
+for (i = num1; i <= num2; i++){
+suma += (i);
+}
+
+return suma;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
