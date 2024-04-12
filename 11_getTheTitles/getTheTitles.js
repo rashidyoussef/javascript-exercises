8f600d9a7f5fda780b3b07d5e18e526ce99d9fd4
rashidyoussef <rashid_gaber@hotmail.com>
2024-04-12 17:36:39 -0600

    falta el de oldest

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(libro) {
+    const libros = libro.map(libro => libro.title)
+    return libros
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+const ordenado = people.sort(function(a, b){
+const lastguy = (a.yearOfDeath - a.yearOfBirth)
+const nextguy = (b.yearOfDeath - b.yearOfBirth)
+return lastguy > nextguy ? 1 : -1
+})
+return oldest
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+/*describe('findTheOldest', () => {
+  test('finds the person with the greatest age!', () => {
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+    expect(findTheOldest(people).name).toBe('Ray');
+*/
